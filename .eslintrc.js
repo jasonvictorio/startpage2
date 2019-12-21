3ea1c858e6aa34f19d264eaff9965b9742375137
Jason Victorio <jgvictorio@gmail.com>
2019-12-21 14:27:53 +1300

    eslint: require trailing comma

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,12 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
+    "comma-dangle": [2, {
+      "arrays": "always-multiline",
+      "objects": "always-multiline",
+      "imports": "always-multiline",
+      "exports": "always-multiline",
+      "functions": "always-multiline"
+    }]
   }
 }
